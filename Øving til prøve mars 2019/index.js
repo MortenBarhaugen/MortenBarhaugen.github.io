ecacf82643b4aca21e0f01d8508f83b6ca244b01
MortenBarhaugen <42533141+MortenBarhaugen@users.noreply.github.com>
2019-04-02 18:48:04 +0200

    ...

---
+++ Øving til prøve mars 2019/index.js
@@ -0,0 +1,21 @@
+// HTML-elementer
+const main = document.querySelector("main");
+// databaseURL
+const db = firebase.database();
+const prosjekter = db.ref("fortfolio/prosjekter")
+function visProsjekt(snap){
+  const key="snap.key"
+  const prosjekt = snap.val();
+  const bilder = prosjekt.bilder;
+  const indeks = prosjekt.forsidebilde;
+  main.innerHTML += `
+  <article style="background-image: url('${bilder[indeks]}')">
+  <h1>${prosjekt.tittel}</h1>
+  <p>${prosjekt.beskrivelse}</p>
+  <a href="prosjekt.html?pid=${key}">Les mer</a>
+
+  </article>
+  `;
+}
+// Event Listener
+prosjekter.on("child_added", visProsjekt);
